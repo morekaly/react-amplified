2bcf58e9e53a47aec73af18b1ba32d8d964bb6ef
More <morekaly@38f9d33fedcc.ant.amazon.com>
2022-03-10 15:24:30 -0600

    added auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
+import { Amplify } from 'aws-amplify';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+//import { Authenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+function App({ isPassedToWithAuthenticator, signOut, user }) {
+  if (!isPassedToWithAuthenticator) {
+    throw new Error(`isPassedToWithAuthenticator was not provided`);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
+    <main>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </main>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
+
+export async function getStaticProps() {
+  return {
+    props: {
+      isPassedToWithAuthenticator: true,
+    },
+  };
+}
